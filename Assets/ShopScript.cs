113b7ed5138c243922a2a40b715745515e163b98
Cosmo48 <59401027+Cosmo48@users.noreply.github.com>
2020-04-19 17:31:11 -0400

    Added Variable Shop

--- Assets/ShopScript.cs
+++ Assets/ShopScript.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
 {
+    public GameObject imageList;
+    public GameObject rawImageTemplate;
 
-    bool isOver;
+    [System.Serializable]
+    public class MultiDimensionalInt {
+        public GameObject[] items;
+    }
+
+    public MultiDimensionalInt[] rounds;
+
+    // round[row].items[column]
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameState.Instance.OnGameStart += CreateMenu;
+
+        Debug.Log(GameState.Instance.Round);
+        foreach (GameObject item in rounds[GameState.Instance.Round - 1].items) {
+            SpriteRenderer sr = item.GetComponent<SpriteRenderer>();
+            GameObject newItem = Instantiate(rawImageTemplate);
+            RawImage riUI = newItem.GetComponent<RawImage>();
+            riUI.texture = sr.sprite.texture;
+            riUI.transform.SetParent(imageList.transform, false);
+        }
+
+
     }
 
     // Update is called once per frame
@@ -20,16 +41,16 @@ public class ShopScript : MonoBehaviour
 
     }
 
-    private void OnMouseOver() {
-
-        transform.position += Vector3.right * 10;
-
-    }
-
-    private void OnMouseExit() {
-        
-
-
+    void CreateMenu() {
+        /*
+        foreach(GameObject item in rounds[GameState.Instance.Round - 1].items) {
+            SpriteRenderer sr = item.GetComponent<SpriteRenderer>();
+            GameObject newItem = Instantiate(rawImageTemplate);
+            RawImage riUI = newItem.GetComponent<RawImage>();
+            riUI.texture = sr.sprite.texture;
+            riUI.transform.SetParent(imageList.transform, false);
+        }
+        */
     }
 
 
