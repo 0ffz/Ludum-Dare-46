352d00766eb116450b663a7adc3af2871ca5a989
0ffz <dan.voznyy@gmail.com>
2020-04-20 17:24:39 -0400

    Camera boundary

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -9,6 +9,7 @@ public class CameraMover : MonoBehaviour {
     public float minCamSize;
     public float maxCamSize;
     private Vector3 _initialPosition;
+
     private Vector3 MousePos {
         get {
             var cam = _cam.ScreenToWorldPoint(Input.mousePosition);
@@ -16,6 +17,8 @@ public class CameraMover : MonoBehaviour {
             return cam;
         }
     }
+
+    public SpriteRenderer background;
 
     private Camera _cam;
     private bool _ignoreInput = true;
@@ -27,10 +30,7 @@ public class CameraMover : MonoBehaviour {
             var originalPos = transform.position;
             pos.z = originalPos.z;
             transform.DOMove(pos, 1.5f).SetEase(Ease.InOutCubic).OnComplete(() => {
-                transform.DOMove(originalPos, 1).SetEase(Ease.InOutCubic).OnComplete(() => {
-                    _ignoreInput = false;
-                    _initialPosition = MousePos;
-                });
+                transform.DOMove(originalPos, 1).SetEase(Ease.InOutCubic).OnComplete(EnableMovement);
             });
         };
     }
@@ -44,15 +44,39 @@ public class CameraMover : MonoBehaviour {
     void Update() {
         if (_ignoreInput) return;
 
+
         if (Input.GetMouseButtonDown(1)) {
             _initialPosition = MousePos;
             return;
         }
-        
+
+        // background.bounds.min
+        if (transform.position.x > background.bounds.max.x) {
+            _ignoreInput = true;
+            transform.DOMoveX(background.bounds.max.x - 5, 0.3f).OnComplete(EnableMovement);
+        }
+        else if (transform.position.x < background.bounds.min.x) {
+            _ignoreInput = true;
+            transform.DOMoveX(background.bounds.min.x + 5, 0.3f).OnComplete(EnableMovement);
+        }
+        if (transform.position.y > background.bounds.max.y) {
+            _ignoreInput = true;
+            transform.DOMoveY(background.bounds.max.y - 5, 0.3f).OnComplete(EnableMovement);
+        }
+        else if (transform.position.y < background.bounds.min.y) {
+            _ignoreInput = true;
+            transform.DOMoveY(background.bounds.min.y + 5, 0.3f).OnComplete(EnableMovement);
+        }
+
         if (Input.GetMouseButton(1))
             transform.position += _initialPosition - MousePos;
 
         _cam.orthographicSize = Mathf.Clamp(_cam.orthographicSize + Input.mouseScrollDelta.y * scrollSens * -1,
             minCamSize, maxCamSize);
+    }
+
+    void EnableMovement() {
+        _ignoreInput = false;
+        _initialPosition = MousePos;
     }
 }
